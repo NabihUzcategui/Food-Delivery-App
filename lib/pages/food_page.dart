157f71c9e304797c4b810139c6cfb000d54c637c
Nabih Uzcategui <nabihuzcateguip@gmail.com>
2024-06-09 20:04:56 -0300

    actualización addons

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -16,17 +16,72 @@ class _FoodPageState extends State<FoodPage> {
       body: Column(
         children: [
           // food image
-          Center(
-            child: Image.asset(widget.food.imagePath),
+          SizedBox(
+            width: 420,
+            child: Image.asset(
+              widget.food.imagePath,
+              fit: BoxFit.cover,
+            ),
           ),
 
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.food.name,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                // food price
+                Text(
+                  '\$ ${widget.food.price}',
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 16),
+                ),
+                const SizedBox(height: 10),
+
+                // food description
+                Text(
+                  widget.food.description,
+                ),
+                const SizedBox(height: 10),
+
+                Text(
+                  "Add-ons",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                // addons
+                ListView.builder(
+                  itemCount: widget.food.availableAddons.length,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    Addon addons = widget.food.availableAddons[index];
+                    return CheckboxListTile(
+                      title: Text(addons.name),
+                      subtitle: Text(addons.price.toString()),
+                      value: false,
+                      onChanged: (value) {},
+                    );
+                  },
+                )
+
+                // buttons -> add to cart
+              ],
+            ),
+          ),
           // food name
-
-          // food description
-
-          // addons
-
-          // buttons -> add to cart
         ],
       ),
     );
