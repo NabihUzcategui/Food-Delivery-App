f5bcb031ecb5c50d70b5a4f2a059773e089555ae
Nabih Uzcategui <nabihuzcateguip@gmail.com>
2024-08-22 16:25:57 -0300

    actualizacón login

--- lib/components/my_cart_title.dart
+++ lib/components/my_cart_title.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_app/components/my_quantity_selector.dart';
+import 'package:flutter_food_delivery_app/models/food.dart';
 import 'package:flutter_food_delivery_app/models/restaurant.dart';
 
 import 'package:provider/provider.dart';
@@ -18,24 +19,27 @@ class MyCartTitle extends StatelessWidget {
           color: Theme.of(context).colorScheme.secondary,
           borderRadius: BorderRadius.circular(8),
         ),
-        margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
         child: Column(
           children: [
             Padding(
+              
               padding: const EdgeInsets.all(8.0),
               child: Row(
+
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   //food image
                   ClipRRect(
                     borderRadius: BorderRadius.circular(12),
                     child: Image.asset(
                       cartItem.food.imagePath,
-                      height: 100,
-                      width: 100,
+                      height: 80,
+                      width: 80,
                     ),
                   ),
 
-                  const SizedBox(width: 10),
+                  const SizedBox(width: 8),
 
                   // name and price
                   Column(
@@ -44,7 +48,11 @@ class MyCartTitle extends StatelessWidget {
                       //Food name
                       Text(cartItem.food.name),
                       //Food priceß
-                      Text('\$${cartItem.food.price}'),
+                      Text(
+                        '\$${cartItem.food.price}',
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
                     ],
                   ),
                   const Spacer(),
@@ -67,6 +75,47 @@ class MyCartTitle extends StatelessWidget {
               ),
             ),
             //addons
+            SizedBox(
+              height: cartItem.selectedAddons.isEmpty ? 0 : 60,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.only(
+                  left: 10,
+                  right: 10,
+                  bottom: 10,
+                ),
+                children: cartItem.selectedAddons
+                    .map(
+                      (addon) => Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child: FilterChip(
+                          label: Row(
+                            children: [
+                              //addon name
+                              Text(addon.name),
+
+                              //addon price
+                              Text(' (\$${addon.price})'),
+                            ],
+                          ),
+                          onSelected: (value) {},
+                          shape: StadiumBorder(
+                            side: BorderSide(
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                          ),
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary,
+                          labelStyle: TextStyle(
+                            color: Theme.of(context).colorScheme.inversePrimary,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
           ],
         ),
       ),
